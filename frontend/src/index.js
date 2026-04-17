5acd33bdf6acd2684f61c0f5509b9df6fa003ce2
revexlogistics-create <https://github.com/revexlogistic-create/revex.git>
2026-04-17 08:38:57 +0100

    fix: toSorted polyfill

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,6 @@
+if (!Array.prototype.toSorted) {
+  Array.prototype.toSorted = function(fn) { return [...this].sort(fn); };
+}
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
